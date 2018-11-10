26cce00469874e9cb434a7a5bdaea052a96cd617
Amy <yeminyi@gmail.com>
2018-11-10 23:53:42 +1100

    taskmanager menu and layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { MatToolbarModule} from '@angular/material';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
   ],
   exports: [
     HeaderComponent,
