73edd39608f794bd21ea9d3be74cac6609f77d04
Amy <yeminyi@gmail.com>
2018-11-19 23:43:05 +1100

    card css for login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: []
+  styles:['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
