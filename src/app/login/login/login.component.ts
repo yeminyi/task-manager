3ea7193336bdc39251aac9cf7ef4c4a491413433
Amy <yeminyi@gmail.com>
2019-01-06 13:56:58 +1100

    login page

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles:['./login.component.scss']
+  styles:[]
 })
 export class LoginComponent implements OnInit {
 
