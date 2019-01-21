2b55fa407a98a5dff768620663f7375c0f187460
Amy <yeminyi@gmail.com>
2019-01-21 23:44:30 +1100

    wip task dialog css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { OverlayContainer } from '@angular/cdk/overlay';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   private _dark = false;
+  constructor(private oc:OverlayContainer){}
   get dark() {
     return this._dark;
   }
   switchDarkTheme(dark: boolean) {
     this._dark = dark;
+    this.oc.getContainerElement().classList.add( dark?'myapp-dark-theme':null);
   }
 }
