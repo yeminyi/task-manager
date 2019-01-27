99142f3f090b8bab69c9b8f578fd9e89acd73231
Amy <yeminyi@gmail.com>
2019-01-27 18:11:58 +1100

    task item animaations

--- src/app/task/task-item/task-item.component.ts
+++ src/app/task/task-item/task-item.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit ,Input,Output,EventEmitter} from '@angular/core';
-
+import { Component, OnInit ,Input,Output,EventEmitter,HostListener} from '@angular/core';
+import { itemAnim } from "../../anims/item.anim";
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss']
+  styleUrls: ['./task-item.component.scss'],
+  animations:[itemAnim]
 })
 export class TaskItemComponent implements OnInit {
 @Input() item;
 @Input() avatar;
 @Output() itemClick=new EventEmitter<void>();
+widerPriority="in";
   constructor() { }
 
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.widerPriority = 'out';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.widerPriority = 'in';
+  }
   ngOnInit() {
     this.avatar=this.item.owner ? this.item.owner.avatar:'unassigned';
   }
