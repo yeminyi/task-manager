4d93aa99acd2969e8e246d1d22f8ccb32bbd4478
Amy <yeminyi@gmail.com>
2019-02-10 00:36:28 +1100

    WIP img list select

--- src/app/shared/img-list-select/img-list-select.component.ts
+++ src/app/shared/img-list-select/img-list-select.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, forwardRef, Input, Output} from '@angular/core';
+import {ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-img-list-select',
   templateUrl: './img-list-select.component.html',
-  styleUrls: ['./img-list-select.component.scss']
+  styleUrls: ['./img-list-select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ImgListSelectComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => ImgListSelectComponent),
+      multi: true,
+    }
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ImgListSelectComponent implements OnInit {
+export class ImgListSelectComponent implements ControlValueAccessor {
 
-  constructor() { }
+  selected: string;
+  @Input() title = 'Please choose a cover';
+  @Input() items: string[] = [];
+  @Input() cols = 8;
+  @Input() rowHeight = '64px';
+  @Input() itemWidth = '80px';
+  @Input() useSvgIcon = false;
+  @Output('itemChange') itemChange = new EventEmitter<string>();
 
-  ngOnInit() {
+  private propagateChange = (_: any) => {};
+
+  public writeValue(obj: any) {
+    if (obj && obj !== '') {
+      this.selected = obj;
+    }
+  }
+
+  public registerOnChange(fn: any) {
+    this.propagateChange = fn;
+  }
+
+  public validate(c: FormControl) {
+    return this.selected ? null : {
+      imageListSelect: {
+        valid: false,
+      },
+    };
+  }
+
+  // todo touched 
+  public registerOnTouched() {
+  }
+
+
+  onChange(i) {
+    this.selected = this.items[i];
+    this.propagateChange(this.items[i]);
+    this.itemChange.emit(this.items[i]);
   }
 
 }
