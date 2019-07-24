58d524c31912b96b76628302c9e02425c67a1105
Amy <yeminyi@outlook.com>
2019-07-24 23:45:19 +1000

    wip quick task

--- src/app/task/components/quick-task.ts
+++ src/app/task/components/quick-task.ts
@@ -1,10 +1,10 @@
 import { Component, Output, EventEmitter, HostListener, ChangeDetectionStrategy } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-quick-task',
   template: `
-    <mat-form-field class="full-width" [matTooltip]="'回车或者点击右侧图标确认'">
-      <input matInput placeholder="在这里快速建立一个任务" [(ngModel)]="desc"/>
+    <mat-form-field class="full-width" [matTooltip]="quickTooltip">
+      <input matInput placeholder="{{'task.quicktask' | translate}}" [(ngModel)]="desc"/>
       <button mat-icon-button matSuffix (click)="sendQuickTask()">
         <mat-icon>send</mat-icon>
       </button>
@@ -16,9 +16,14 @@ import { Component, Output, EventEmitter, HostListener, ChangeDetectionStrategy 
 export class QuickTaskComponent {
 
   desc: string;
+  quickTooltip: string;
   @Output() quickTask = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private translate:TranslateService) { }
+
+  ngDoCheck(){
+    this.quickTooltip = this.translate.instant('task.quicktooltip');
+  }
 
   @HostListener('keyup.enter')
   sendQuickTask() {
